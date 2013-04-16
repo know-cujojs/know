ea506229d6c1ab2623a2230e9684351aa1a69088
Scott Andrews <scothis@gmail.com>
2013-04-16 12:36:36 -0400

    support CF v2

--- generate.js
+++ generate.js
@@ -1,7 +1,7 @@
 // shim to run on Cloud Foundry
 require('docpad').createInstance({ env: 'production' }, function(err, docpadInstance) {
 	if (err) { return console.log(err.stack); }
-	docpadInstance.action('generate server', function (err, result){
+	docpadInstance.action('generate', function (err, result){
 		if (err) { return console.log(err.stack); }
 		console.log('OK');
 	});
