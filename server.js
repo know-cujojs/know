d81a0a0f2f2c22c738629b15c1cb5eb0d6122eda
Scott Andrews <scothis@gmail.com>
2013-04-15 20:19:21 -0400

    polish Cloud Foundry start script

--- server.js
+++ server.js
@@ -1,2 +1,7 @@
 // shim to run on Cloud Foundry
-require('child_process').spawn('docpad', ['server', '--env', 'production'], { stdio: 'inherit' });
+var path = require('path'),
+    fs = require('fs'),
+    child = require('child_process'),
+    cmd = path.join('node_modules', 'docpad', 'bin', 'docpad-server');
+fs.chmodSync(cmd, '755');
+child.spawn(cmd, ['--env', 'production'], { stdio: 'inherit' });
